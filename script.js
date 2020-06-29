e44b64695426f4eb6a030bd426de025c6c57903a
Ashi Sinha <56842767+ashiabcd@users.noreply.github.com>
2020-06-29 17:49:56 +0530

    Update script.js
    Made radio buttons disabled once you click on start.
    Added an if statement in startGame() function in case first player is computer.
    In the main branch ,till now we only have human player in master branch so added the AI computer part as well but the code is complete only till the easy part of the game where the AI player makes a random move and only that part is done in the bestSpot() function till now.

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ const pointer = document.querySelectorAll('.cell');
 startGame();
 
 function startGame() {
+	document.getElementById("r1").disabled = true;
+	document.getElementById("r2").disabled = true;
+	document.getElementById("r3").disabled = true;
+	document.getElementById("r4").disabled = true;
+	document.getElementById("r5").disabled = true;
 	document.querySelector(".endgame").style.display = "none";
 	Board = Array.from(Array(9).keys());
 	for (var i = 0; i < pointer.length; i++) {
@@ -24,13 +29,109 @@ function startGame() {
 		pointer[i].style.removeProperty('background-color');
 		pointer[i].addEventListener('click', switchonClick, false);
 	}
+  
+   if(document.getElementById("r2").checked == true)
+    { var p=randomNumber(0, 8) 
+      Board[p] = AIplayer;
+	  document.getElementById(p).innerText = AIplayer;
+    }
+ 
 }
+ 
+
 
 function switchonClick(square) {
-	turn(square.target.id, human)
+	   if(typeof Board[square.target.id]=='number' && !checkTie()&&!checkWin(Board,AIplayer))
+		{ turn(square.target.id, human)
+		  if (!checkTie()&&!checkWin(Board,human)) turn(bestSpot(), AIplayer);
+	    }
 }
 
 function turn(squareId, player) {
 	Board[squareId] = player;
 	document.getElementById(squareId).innerText = player;
+	let gameWon = checkWin(Board, player)
+	if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) => 
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winningcombinations.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function gameOver(gameWon) {
+	for (let index of winningcombinations[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor =
+			gameWon.player == human ? "blue" : "red";
+	}
+	for (var i = 0; i < pointer.length; i++) {
+		pointer[i].removeEventListener('click', switchonClick, false);
+	}
+  
+
+  declareWinner(gameWon.player == human ? "You win!" : "You lose.");
+      document.getElementById("r1").removeAttribute("disabled"); 
+      document.getElementById("r2").removeAttribute("disabled"); 
+      document.getElementById("r3").removeAttribute("disabled"); 
+      document.getElementById("r4").removeAttribute("disabled"); 
+      document.getElementById("r5").removeAttribute("disabled");   
+	
+}
+
+function declareWinner(who) {
+	document.querySelector(" .endgame").style.display = "block";
+	document.querySelector(" .endgame .text").innerText = who;
+}
+
+function emptySquares() {
+	return Board.filter(s => typeof s == 'number');
+}
+
+
+
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < pointer.length; i++) {
+			pointer[i].style.backgroundColor = "green"; 
+			pointer[i].removeEventListener('click', switchonClick, false);
+		}
+		declareWinner("Tie Game!");
+      document.getElementById("r1").removeAttribute("disabled"); 
+      document.getElementById("r2").removeAttribute("disabled"); 
+      document.getElementById("r3").removeAttribute("disabled"); 
+      document.getElementById("r4").removeAttribute("disabled"); 
+      document.getElementById("r5").removeAttribute("disabled"); 
+		return true;
+	}
+	return false;
+}
+
+function randomNumber(min, max) {  
+    min = Math.ceil(min); 
+    max = Math.floor(max); 
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+}  
+
+
+function bestSpot() {
+	
+
+  if(document.getElementById("r3").checked = true)
+   {
+	var available=emptySquares(Board);
+	return available[randomNumber(0,available.length-1)];
+   }
+ 
+  
+
+
 }
